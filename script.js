fad363291fec95c0489262cc3aab98c47ae75d03
ecandotti <enzo.candotti16@hotmail.com>
2021-02-23 15:27:38 +0100

    step 2

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let numOne = "3"
-let numTwo = "2"
+// Step one
 
-console.log(parseInt(numOne) + parseInt(numTwo))
+let number1 = "3"
+let number2 = "2"
+
+console.log(parseInt(numOne) + parseInt(numTwo) + '')
+
+// Step two
+
+function add(num1 = 0, num2 = 0, num3 = 0) {
+    return num1 + num2 + num3
+}
+
+console.log(2,4)
