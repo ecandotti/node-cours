f25409cfa3dca0ee25b458222a908192446deda2
ecandotti <enzo.candotti16@hotmail.com>
2021-02-23 15:45:31 +0100

    update

--- script.js
+++ script.js
@@ -3,13 +3,23 @@
 let number1 = "3"
 let number2 = "2"
 
-console.log(parseInt(number1) + parseInt(number2) + '')
-console.log(typeof (parseInt(number1) + parseInt(number2) + ''))
+console.log(parseInt(number1) + parseInt(number2) + "")
+console.log(typeof (parseInt(number1) + parseInt(number2) + ""))
 
 // Step two
 
-function add(num1 = 0, num2 = 0, num3 = 0) {
-    return parseInt(num1) + parseInt(num2) + parseInt(num3) + ''
+function add(numbers) {
+    if (numbers.length === 0) return "0"
+
+    const arrNumber = numbers.split(",")
+    let result = 0
+
+    for (let i = 0; i < arrNumber.length; i++) {
+        result += parseInt(arrNumber[i])
+        
+    }
+    
+    return (result + "")
 }
 
-console.log(add(2,4))
+console.log(add("23,33,1"))
